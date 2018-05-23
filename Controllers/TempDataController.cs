e2fab9f70948b83e72e2be793362f68e68c4bef3
famattsson <38214259+famattsson@users.noreply.github.com>
2018-05-23 17:54:02 +0200

    Add files via upload

---
+++ Controllers/TempDataController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Logic;
+using System.Web.Mvc;
+
+namespace Presentation.Controllers
+{
+    public class TempDataController : Controller
+    {
+        // GET: TempData
+        public ActionResult GetData(string fromDate, string toDate)
+        {
+            List<string> locations = new List<string>();
+            if(Session["locations"] != null)
+            {
+                foreach (string location in ((string)Session["locations"]).Split(','))
+                {
+                    locations.Add(location);
+                }
+            }
+            return View("TempDataView", TempDataHandler.GetRecords(fromDate, toDate, locations));
+        }
+
+        public ActionResult ChooseLocation(string id)
+        {
+            Session["locations"] = id;
+            return new EmptyResult();
+        }
+    }
+}
